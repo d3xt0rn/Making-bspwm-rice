b247d33925db462c102379577f64fac13ce0021a
/home/dubina <just tell me in discord>
2026-07-24 05:43:17 +0300

    pls dont "six seven"

--- config/dmenu/scripts/powermenu.sh
+++ config/dmenu/scripts/powermenu.sh
@@ -6,6 +6,6 @@ chosen=$(echo -e "$options" | dmenu -i -p "Power:")
 
 case "$chosen" in
 Logout) bspc quit ;;
-Restart) doas reboot ;;
-Shutdown) doas poweroff ;;
+Restart) loginctl reboot ;;
+Shutdown) loginctl poweroff ;;
 esac
