47c41e3b795f09277104ab1a9b7f331b12b70ea9
/home/dubina <just tell me in discord>
2026-07-24 14:06:23 +0300

    idk what type here x35

--- config/polybar/scripts/bluetooth.sh
+++ config/polybar/scripts/bluetooth.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 if ! bluetoothctl show | grep -q "Powered: yes"; then
-  echo "B: off"
+  echo "Bt: off"
   exit 0
 fi
 
 connected=$(bluetoothctl devices Connected)
 
 if [ -z "$connected" ]; then
-  echo "B: on"
+  echo "Bt: on"
 else
   name=$(echo "$connected" | head -n1 | cut -d' ' -f3-)
   echo " $name"
